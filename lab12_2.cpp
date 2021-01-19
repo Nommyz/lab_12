76e1c3646b9a523cece4970a75c045946faec041
Nommyz <76035501+Nommyz@users.noreply.github.com>
2021-01-19 20:05:05 +0700

    lab12.2 completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,54 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void showImage(const bool a[][M]) 
+{
+    int i,j;
+    for(j=0;j<72;j++)
+    {
+        cout << '-';
+    }
+    cout << '\n';
+    for(i=0;i<30;i++)
+        for(j=0;j<71;j++)
+        {
+            switch(j)
+            {
+                case 0:
+                    cout << '|';
+                    break;
+                case 70:
+                    cout << '|';
+                    cout << '\n';
+                    continue;
+                    
+            }
+            if(a[i][j]==0)
+            {
+                cout << ' ';
+            }
+            else
+            {
+                cout << '*';
+            }
+            
+        }
+      for(j=0;j<72;j++)
+    {
+        cout << '-';
+    }
+   cout << "\n";
+}
+void updateImage(bool a[][M],int b,int c,int d)
+{
+    int i,j;
+    for(i=0;i<N;i++)
+     for(j=0;j<M;j++)
+     {
+        if(sqrt(pow((i-c),2)+pow((j-d),2)) <= b-1)
+        {
+            a[i][j] = 1;
+        }
+      
+     }
+}
