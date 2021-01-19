b999eb150f7f2f2beb0403b63233b3a470c60093
Nommyz <76035501+Nommyz@users.noreply.github.com>
2021-01-19 20:03:02 +0700

    lab 12.1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,37 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[])
+{
+    int i;
+    double sum=0,sum2=0,sum3=1,sum4=0,max,min;
+   
+    for(i=0;i<n;i++)
+    {
+        sum = sum + a[i];
+        sum2 = sum2+pow(a[i],2);
+        sum3 = sum3*a[i];
+        sum4 = sum4+1/a[i];
+    }
+    b[0] = sum/n;
+    b[1] = (sqrt((sum2/n)-pow(b[0],2))); 
+    b[2] = pow(sum3,(float)1/n);
+    b[3] = n/sum4;
+    max = a[0];
+    min = a[0];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max = a[i];
+        }
+        if(a[i]<min)
+        {
+            min = a[i];
+        }
+    }
+    b[4] = max;
+    b[5] = min;
+
+}
